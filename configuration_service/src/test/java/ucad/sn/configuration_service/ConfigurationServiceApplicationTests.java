c396e1981d263f2d200a5e6e24f12cd3ddda3c9b
Mamadou SENE <senemamadou1999@gmail.com>
2024-09-06 06:46:48 +0000

    ajout du micro-service de configuration dans le repos

---
+++ configuration_service/src/test/java/ucad/sn/configuration_service/ConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ucad.sn.configuration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
