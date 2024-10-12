d2d646b9da1900e234374f435cbd84406b8a32d4
Mamadou SENE <senemamadou1999@gmail.com>
2024-10-12 21:48:56 +0000

    ajout du service de configuration

--- configuration_service/src/main/java/ucad/sn/configuration_service/ConfigurationServiceApplication.java
+++ configuration_service/src/main/java/ucad/sn/configuration_service/ConfigurationServiceApplication.java
@@ -2,8 +2,10 @@ package ucad.sn.configuration_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigurationServiceApplication {
 
     public static void main(String[] args) {
