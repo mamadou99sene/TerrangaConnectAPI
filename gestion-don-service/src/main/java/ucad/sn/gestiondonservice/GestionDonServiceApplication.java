49ee2aa50c07c92d8f160917fe6742f8975278cd
Mamadou SENE <senemamadou1999@gmail.com>
2024-10-12 21:52:28 +0000

    conf du service de gestion de dons avec sprin cloud conf

--- gestion-don-service/src/main/java/ucad/sn/gestiondonservice/GestionDonServiceApplication.java
+++ gestion-don-service/src/main/java/ucad/sn/gestiondonservice/GestionDonServiceApplication.java
@@ -2,10 +2,13 @@ package ucad.sn.gestiondonservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import ucad.sn.gestiondonservice.config.GlobalConfig;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableConfigurationProperties(GlobalConfig.class)
 public class GestionDonServiceApplication {
 
     public static void main(String[] args) {
