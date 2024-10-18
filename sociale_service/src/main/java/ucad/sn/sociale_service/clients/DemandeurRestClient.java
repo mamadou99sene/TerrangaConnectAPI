fc471492806b2dbc4f3de7cd44eca488462af7d5
Mamadou SENE <senemamadou1999@gmail.com>
2024-10-18 23:31:35 +0000

    resistance aux pannes avec resilience4j et impléméntation du chaching

--- sociale_service/src/main/java/ucad/sn/sociale_service/clients/DemandeurRestClient.java
+++ sociale_service/src/main/java/ucad/sn/sociale_service/clients/DemandeurRestClient.java
@@ -1,5 +1,8 @@
 package ucad.sn.sociale_service.clients;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +11,18 @@ import ucad.sn.sociale_service.models.Demandeur;
 @FeignClient("utilisateur-service")
 public interface DemandeurRestClient {
     @GetMapping("/api/utilisateurs/{id}")
-
+    @CircuitBreaker(name = "UtilisateurDeclaration",fallbackMethod = "getCachedUtilisateur")
+    @Retry(name = "retrySearchUtilisateur",fallbackMethod = "getCachedUtilisateur")
+    @Cacheable(value = "utilisateurById", key = "#id")
     public Demandeur findUtilisateurById( @PathVariable String id);
+    default Demandeur getCachedUtilisateur(String id, Exception e)
+    {
+        Demandeur demandeur=new Demandeur();
+        demandeur.setId(id);
+        demandeur.setEmail("availaible");
+        demandeur.setScore(0);
+        demandeur.setProfile("undefine");
+        demandeur.setTelephone("undefine");
+        return demandeur;
+    }
 }
