707f3705a42bf58b6f8afa1f86dff1051035be88
Mamadou SENE <senemamadou1999@gmail.com>
2024-12-01 21:07:35 +0000

    dashboard

--- admin-web/src/app/app.routes.ts
+++ admin-web/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-  {path:'', component:HomePageComponent}
+  {path:'', component:DashboardComponent}
 ];
