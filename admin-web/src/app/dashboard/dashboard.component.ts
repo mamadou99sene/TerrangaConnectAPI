628343976add9724d78b61982c46875e05e0eb4d
Mamadou SENE <senemamadou1999@gmail.com>
2024-12-12 16:05:48 +0000

    admin evenement et demande sang

--- admin-web/src/app/dashboard/dashboard.component.ts
+++ admin-web/src/app/dashboard/dashboard.component.ts
@@ -1,69 +1,26 @@
 import {Component, OnInit} from '@angular/core';
 import {DeclarationService} from '../services/declaration.service';
-import {UrgenceSociale} from '../../models/UrgenceSociale';
-import {Evenement} from '../../models/Evenement';
-import {Demande_don_sang} from '../../models/Demande_don_sang';
 import {CommonModule} from '@angular/common';
 import { AppbarComponent } from "../appbar/appbar.component";
 import { SidebarComponent } from "../sidebar/sidebar.component";
+import { UrgenceComponent } from "../urgence/urgence.component";
+import { DemandesangComponent } from "../demandesang/demandesang.component";
+import { EvenementComponent } from "../evenement/evenement.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone:true,
-  imports: [CommonModule, AppbarComponent, SidebarComponent],
+  imports: [CommonModule, AppbarComponent, SidebarComponent, UrgenceComponent, DemandesangComponent, EvenementComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent implements OnInit{
-  listUrgences:Array<UrgenceSociale>=[];
-  listEvenements:Array<Evenement>=[];
-  listDemandeSang:Array<Demande_don_sang>=[];
+
   constructor(private declarationService :DeclarationService) {
   }
   ngOnInit() {
-    this.loadAdminAllUrgencesSociale();
+
   }
-  loadAdminAllUrgencesSociale()
-  {
-    this.declarationService.getAdminAllUrgencesSociale().subscribe({
-      next:(data)=>{
-        this.listUrgences=data;
-      },error:err => {
-        console.log(err);
-      }
-    })
-  }
-  loadAdminAllEvenement()
-  {
-    this.declarationService.getAdminAllEvenement().subscribe({
-      next:(data)=>{
-       this.listEvenements=data;
-      },error:err => {
-        console.log(err);
-      }
-    })
-  }
-  loadAdminAllDemandeDonSang()
-  {
-    this.declarationService.getAdminAllDemandeDonSang().subscribe({
-      next:(data)=>{
-       this.listDemandeSang=data;
-      },error:err => {
-        console.log(err);
-      }
-    })
-  }
+ 
   
-  valider(urgence: UrgenceSociale) {
-    const validation=confirm("Voulez vous vraiment valider cette declaration d'urgence ?");
-    if(validation)
-    this.declarationService.validerUrgence(urgence).subscribe({
-      next:(validedUrgence)=>{
-      console.log(validedUrgence);
-      }, error(err) {
-        console.log("++++++++++++++++++++++++++");
-        console.log(err);
-      },
-    });
-    }
 }
