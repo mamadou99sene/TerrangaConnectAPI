b768de280be54682d713f21ac1a03611dd933d68
mamadou.sene <mamadou.sene@groupeafrika.com>
2025-05-17 16:52:24 +0000

    securité apres incident

--- utilisateur_service/src/main/java/ucad/sn/utilisateur_service/config/SecurityConfig.java
+++ utilisateur_service/src/main/java/ucad/sn/utilisateur_service/config/SecurityConfig.java
@@ -2,22 +2,49 @@ package ucad.sn.utilisateur_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    private JwtAuthConverter jwtAuthConverter;
+
+    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
+
     @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                //.authorizeHttpRequests(ar->ar.requestMatchers("/products/**").permitAll())
+                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .oauth2ResourceServer(o2->o2.jwt(jwt->jwt.jwtAuthenticationConverter(jwtAuthConverter)))
+                .headers(h->h.frameOptions(fo->fo.disable()))
+                .csrf(csrf->csrf.ignoringRequestMatchers("/h2-console/**"))
+                .build();
+    }
+
+    /*@Bean
     public PasswordEncoder passwordEncoder()
     {
         return new BCryptPasswordEncoder();
-    }
-    @Bean
+    }*/
+    /*@Bean
     public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
         return http.csrf().disable().
                 authorizeHttpRequests(authorize ->authorize.anyRequest().permitAll()
@@ -26,12 +53,23 @@ public class SecurityConfig {
         )
                 .build();
 
-        /*
-                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth-> auth.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .oauth2ResourceServer(oauth2->oauth2.jwt(Customizer.withDefaults()))
-                .build();*/
+//                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeHttpRequests(auth-> auth.anyRequest().authenticated())
+//                .httpBasic(Customizer.withDefaults())
+//                .oauth2ResourceServer(oauth2->oauth2.jwt(Customizer.withDefaults()))
+//                .build();
+    }*/
+
+
+    //@Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
